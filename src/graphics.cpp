3b48d3de527e416f6197bea2077efff586da8eb7
QuadlcorE <62242772+QuadlcorE@users.noreply.github.com>
2023-01-18 23:17:10 +0100

    Fixed a compile issue with the make file.

---
+++ src/graphics.cpp
@@ -0,0 +1,14 @@
+#include "graphics.h"
+
+//#include "include/nuklear.h"
+#include <nuklear.h>
+
+// Display window 
+/*int displayWindow() {
+    if (nk_begin(ctx, "Nuklear", nk_rect(WINDOW_WIDTH/2 - 110, WINDOW_HEIGHT/2 - 110, 220, 220),
+                     NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE))
+    {
+            // Widgets code here
+    }
+    nk_end(ctx);
+}*/
--- src/graphics.h
+++ src/graphics.h
@@ -1,1 +1,9 @@
-#include <nuclear.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <nuklear.h>
+
+
+// Display window 
+int displayWindow();
