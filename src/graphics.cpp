87a404e61ba36b37aad574dfe3905c6d94433fed
QuadlcorE <62242772+QuadlcorE@users.noreply.github.com>
2023-01-20 00:22:53 +0100

    Finally got the Makefile to compile things correctly.

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,14 +1,60 @@
 #include "graphics.h"
 
-//#include "include/nuklear.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#define NK_KEYSTATE_BASED_INPUT
+
 #include <nuklear.h>
 
-// Display window 
-/*int displayWindow() {
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+
+ /* Platform */
+    struct nk_glfw glfw = {0};
+    static GLFWwindow *win;
+    int width = 0, height = 0;
+    struct nk_context *ctx;
+    struct nk_colorf bg;
+
+    /* GLFW */
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stdout, "[GFLW] failed to init!\n");
+        exit(1);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+//Display window 
+int displayWindow() {
     if (nk_begin(ctx, "Nuklear", nk_rect(WINDOW_WIDTH/2 - 110, WINDOW_HEIGHT/2 - 110, 220, 220),
                      NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE))
     {
             // Widgets code here
     }
     nk_end(ctx);
-}*/
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include "timeFunctions.h"
+#include "graphics.h"
 #include <iostream>
+//#include "graphics.cpp"
 
 int main(){
     std::cout<<"lets Get this started";
+    int x{0};
+    while(true){
+        std::cout<<"working";
+        ++x;
+        if(x == 10000){
+            displayWindow();
+            break;
+        }
+    }
 }
